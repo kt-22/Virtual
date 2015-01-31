e4bda1f03e14317ab11f2b04dcb357c604f6c994
Admin <Admin@microsof-2cc07d>
2015-01-31 12:13:37 +0200

    No phone

---
+++ Phone/src/Phone/mobile/Mobile.java
@@ -0,0 +1,11 @@
+package Phone.mobile;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobile = "+380(93)000-00-00";
+		System.out.println("Mobile:"+mobile);
+
+	}
+
+}
